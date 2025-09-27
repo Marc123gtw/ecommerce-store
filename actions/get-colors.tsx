515a84eb16f630c7d92051164819ae6b7f110dc9
GTW777 <markpaskar555@gmail.com>
2025-09-27 11:25:23 +0300

    new

--- actions/get-colors.tsx
+++ actions/get-colors.tsx
@@ -1,11 +1,11 @@
 import { Color } from "@/types";
 
-const URL = `${process.env.NEXT_PUBLIC_API_URL}/Colors`;
+const URL = `${process.env.NEXT_PUBLIC_API_URL}/colors`
 
-const getColors= async (): Promise<Color[]> => {
+const getColors = async (): Promise<Color[]> => {
     const res = await fetch(URL);
 
     return res.json();
-};
+}
 
-export default getColors;
+export default getColors; 
